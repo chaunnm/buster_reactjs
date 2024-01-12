1e984560f906c76f086e7c7f3cc828b2dca5610c
nnmchau <nnmchau781@gmail.com>
2024-01-12 22:16:46 +0700

    fix import welcome page

---
+++ src/components/Welcome/index.ts
@@ -0,0 +1,6 @@
+import Intro from './Intro';
+import News from './News';
+import Stream from './Stream';
+import Footer from './Footer';
+
+export { Intro, News, Stream, Footer };
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 import { MovieList, MovieListItem } from './MovieList';
 import Navigation from './Navigation';
+import { Intro, News, Stream, Footer } from './Welcome';
 
-export { MovieList, MovieListItem, Navigation };
+export { MovieList, MovieListItem, Navigation, Intro, News, Stream, Footer };
 
 
